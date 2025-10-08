b90488ebb82e39e30db7a966555750b0c8c6f66d
raychiikk <cherhinetsyulia@gmail.com>
2025-10-08 19:08:13 +0300

    moved to data folder

---
+++ src/JS/data/WeatherCondition.js
@@ -0,0 +1,12 @@
+// клас WeatherCondition є базовим для різних типів погодних умов потрібен в ієрархії успадкування для моделювання поліморфізму
+export class WeatherCondition {
+    #type;
+
+    constructor(type) { this.#type = type; }
+
+    // getter для отримання типу погодних умов 
+    getType() { return this.#type; }
+
+    // нетривіальний метод, який повертає опис умов
+    describe() { return `Condition: ${this.#type}`; }
+}

27a4bdb937a435f17bf545cfa7d7007fe64fddd4
raychiikk <cherhinetsyulia@gmail.com>
2025-10-08 21:41:05 +0300

    open main.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: '/main.js', // Відкриває main.js як точку входу
+  },
+  
 })

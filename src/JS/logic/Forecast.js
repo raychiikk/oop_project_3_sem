6744175668555f0274a25b7cd169e1161e1397cd
raychiikk <cherhinetsyulia@gmail.com>
2025-10-08 19:09:35 +0300

    moved to logic folder

---
+++ src/JS/logic/Forecast.js
@@ -0,0 +1,16 @@
+// клас Forecast управляє списком погодних даних для прогнозу на кілька днів, інкапсцуляція
+export class Forecast {
+  #data = []; // масив об'єктів WeatherData
+
+  // нетривіальний метод для додавання нового дня до прогнозу.
+  addDay(weather) { this.#data.push(weather); }
+
+  // нетривіальний метод, який обчислює середню температуру за всіма днями, використано метод reduce для обробки масиву
+  getAverageTemp() { return this.#data.reduce((sum, w) => sum + w.getTemperature(), 0) / this.#data.length || 0; }
+
+  // нетривіальний метод, який прогнозує тенденцію температури 
+  predictTrend() { return this.#data.length > 0 ? 'Rising' : 'Stable'; }
+
+  // публічний метод для отримання доступу до приватного масиву даних 
+  getData() { return this.#data; }
+}

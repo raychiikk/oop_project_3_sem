2d4789325a39f4ee53095c953fb4cb5b365a3f45
raychiikk <cherhinetsyulia@gmail.com>
2025-10-13 16:40:19 +0300

    add a comment

--- src/JS/logic/Forecast.js
+++ src/JS/logic/Forecast.js
@@ -1,18 +1,17 @@
 // клас Forecast управляє списком погодних даних для прогнозу на кілька днів, інкапсцуляція
 export class Forecast {
-  constructor(days) {
-      this.days = days;
-  }
-
-  averageIntensity() {
-      const sum = this.days.reduce((acc, day) => acc + day.getIntensity(), 0);
-      return (sum / this.days.length).toFixed(2);
-  }
-
-  printForecast() {
-      console.log("📆 Forecast:");
-      this.days.forEach((day, i) => console.log(`Day ${i + 1}: ${day.describe()}`));
-      console.log(`Average intensity: ${this.averageIntensity()}`);
-  }
+constructor(days) {
+    this.days = days;
 }
-  
+
+averageIntensity() {
+    const sum = this.days.reduce((acc, day) => acc + day.getIntensity(), 0);
+    return (sum / this.days.length).toFixed(2);
+}
+
+printForecast() {
+    console.log("📆 Forecast:");
+    this.days.forEach((day, i) => console.log(`Day ${i + 1}: ${day.describe()}`));
+    console.log(`Average intensity: ${this.averageIntensity()}`);
+}
+}

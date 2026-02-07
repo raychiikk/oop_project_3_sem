d34b5d3a2657ac3465814d8e29e94fa5dd1b922f
raychiikk <cherhinetsyulia@gmail.com>
2026-02-07 20:46:41 +0200

    file which implemented by Singleton

--- src/JS/data/CityManager.js
+++ src/JS/data/CityManager.js
@@ -1,18 +1,26 @@
-// src/js/data/CityManager.js
 export class CityManager {
+    static #instance = null; // Статичне поле для зберігання єдиного екземпляра
     #cities = [];
 
-    addCity(city) {
-    if (city.getName().trim() !== '' && !this.#cities.some(c => c.getName() === city.getName())) {
-        this.#cities.push(city);
+    constructor() {
+        // Логіка Singleton: якщо екземпляр вже є, повертаємо його
+        if (CityManager.#instance) {
+            return CityManager.#instance;
+        }
+        CityManager.#instance = this;
     }
+
+    addCity(city) {
+        if (city.getName().trim() !== '' && !this.#cities.some(c => c.getName() === city.getName())) {
+            this.#cities.push(city);
+        }
     }
 
     removeCity(cityToRemove) {
-    this.#cities = this.#cities.filter(city => city.getName() !== cityToRemove.getName());
+        this.#cities = this.#cities.filter(city => city.getName() !== cityToRemove.getName());
     }
 
     getCities() {
-    return [...this.#cities];
+        return [...this.#cities];
     }
 }
